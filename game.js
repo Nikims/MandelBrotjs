9b4e7464f6f47195f7e415d08b5ad978626164ca
clecho52 <nikolayordanov2667@gmail.com>
2021-11-17 20:07:44 +0200

    added color, optimized

--- game.js
+++ game.js
@@ -1,117 +1,74 @@
-// Creating variables
 let myX = 0,
   myY = 0,
   updates = 0;
-scale = 120;
-
-xOffset = 300;
+scale = 300;
+offset = 500;
 coords = [];
-cReal = -4;
-cFake = -0.5;
-alreadyDid = false;
-renderingResolution = Math.round(scale / 140);
-iterations = Math.round(scale / 2);
+cReal = 0;
+cFake = 0;
+virtualMouseX = 0;
+virtualMouseY = 40;
+placeDown = [];
+resolution = 1;
 function transformCoords(i) {
-  return scale * i;
+  return scale * i + 300;
 }
+
 function update() {
-  // Napisanoto tuk se izpulnqva otnovo i otnovo mnogo puti v sekunda
-}
-function plotAPoint(x, i) {
-  context.fillRect(
-    transformCoords(x) + xOffset,
-    transformCoords(-i) + 300,
-    2,
-    2
-  );
-}
-
-function draw() {
-  // tuk naprogramirai kakvo da se risuva
-  if (!alreadyDid) {
-    alreadyDid = true;
-    for (
-      he = -200 / scale;
-      he < 100 / scale;
-      he += renderingResolution / scale
-    ) {
-      for (
-        she = -200 / scale;
-        she < 90 / scale;
-        she += renderingResolution / scale
+  for (j = 0; j < 5000; j++) {
+    coords = [];
+    coords[0] = { x: 0, i: 0 };
+    for (i = 0; i < 100; i++) {
+      coords.push({
+        x: coords[i].x * coords[i].x - Math.pow(coords[i].i, 2) + cReal,
+        i: 2 * (coords[i].x * coords[i].i) + cFake,
+      });
+      if (
+        coords[i].x < 1 &&
+        coords[i].x > -1 &&
+        coords[i].i < 1 &&
+        coords[i].i > -1
       ) {
-        cReal = he;
-        cFake = she;
-
-        coords = [];
-        coords[0] = { x: 0 / scale, i: -(0 / scale) };
-
-        for (i = 0; i < iterations; i++) {
-          coords.push({
-            x: coords[i].x * coords[i].x - Math.pow(coords[i].i, 2) + cReal,
-            i: 2 * (coords[i].x * coords[i].i) + cFake,
-          });
-          /*
-          drawLine(
-            transformCoords(coords[i].x),
-            transformCoords(-coords[i].i),
-            transformCoords(coords[i + 1].x),
-            transformCoords(-coords[i + 1].i)
-          );
-          */
-        }
-
-        if (
-          coords[coords.length - 1].x < 1 &&
-          coords[coords.length - 1].x > -1 &&
-          coords[coords.length - 1].i < 1 &&
-          coords[coords.length - 1].i > -1
-        ) {
-          context.fillRect(
-            transformCoords(cReal) + xOffset,
-            transformCoords(cFake) + 300,
-            renderingResolution,
-            renderingResolution
-          );
-        }
-        for (i = 0; i < 800; i += scale) {
-          context.fillRect(i - 100, 300, 1, 10);
-        }
-        for (i = 0; i < 600; i += scale) {
-          context.fillRect(xOffset, i - 100, 10, 1);
-        }
-        for (i = 0; i < coords.length; i++) {
-          //  plotAPoint(coords[i].x, coords[i].i);
-        }
-        /*for (i = 0; i < 360; i++) {
-      context.fillRect(
-        Math.sin(i) * scale + 300,
-        Math.cos(i) * scale + 300,
-        5,
-        5
-      );
-    }*/
-
-        // plotAPoint(5, 5);
-
-        drawLine(0, 300, 600, 300);
-        drawLine(xOffset, 0, xOffset, 600);
-        context.fillText(-(mouseY - 300) / scale, 30, 30, 300);
+        placeDown[0] = {
+          x: (virtualMouseX - offset) / scale,
+          y: (virtualMouseY - 300) / scale,
+          color: i * 2.5,
+        };
       }
+    }
+
+    lel = coords.length - 1;
+    if (virtualMouseX < offset * 2) {
+      virtualMouseX += 1 / resolution;
+    } else {
+      virtualMouseY += 1 / resolution;
+      virtualMouseX = 0;
+    }
+    cReal = (virtualMouseX - offset) / scale;
+    cFake = -((virtualMouseY - 300) / scale);
+    for (i = 0; i < placeDown.length; i++) {
+      context.fillStyle =
+        "rgb(" +
+        placeDown[i].color * 3 +
+        "," +
+        placeDown[i].color * 1.3 +
+        "," +
+        placeDown[i].color * 2 +
+        ")";
+      context.fillRect(
+        placeDown[i].x * scale + offset,
+        placeDown[i].y * scale + 300,
+        Math.ceil(1 / resolution),
+        Math.ceil(1 / resolution)
+      );
     }
   }
 }
 
+function draw() {}
 function keyup(key) {
   // Show the pressed keycode in the console
   console.log("Pressed", key);
-  /*
-  redraw();
-  scale += 50;
-  renderingResolution = Math.round(scale / 5);
-  iterations = Math.round(scale / 2);
-  alreadyDid = false;
-  */
 }
 
 function mouseup() {
